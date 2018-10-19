c8e89370c489a4c95fa4a2fff6f4e8a4a60966a2
Varazdat Stepanyan <varostep@gmail.com>
2018-10-20 01:13:31 +0400

    Encoder Added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("fok u");
-        System.out.println("fok you too, bich");
+        Encoder encoder = new Encoder();
+        System.out.println(encoder.encode("I love you Sergey"));
     }
 }
